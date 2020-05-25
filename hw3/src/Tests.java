f2e1f668b2d4419d89fec311af008d8c0f27792d
rozenbergidan <idanroze@post.bgu.ac.il>
2020-05-25 20:51:33 +0300

    lo mazhik amrti mashmuti

--- hw3/src/Tests.java
+++ hw3/src/Tests.java
@@ -1,5 +1,30 @@
+import Board.Board;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Tests {
     public static void main(String[]args){
-        System.out.println("hello tsuri");
+        String[] levels={"#########################\n" +
+                "#b..........@..........b#\n" +
+                "#.......................#\n" +
+                "#...###...........###...#\n" +
+                "#.......................#\n" +
+                "#...zzz...........zzz...#\n" +
+                "#...###g...w.w...g###...#\n" +
+                "#.......................#\n" +
+                "#......D.........D......#\n" +
+                "#..####...........####..#\n" +
+                "#..z##z...........z##z..#\n" +
+                "#..####...........####..#\n" +
+                "#.......................#\n" +
+                "########.........########\n" +
+                "########.........########\n" +
+                "########.ww.K.ww.########\n" +
+                "#########################"};
+        Board.getBoard().initBoard(levels);
+        Board.getBoard().selectCharacter(1);
+        Board.getBoard().buildBoard();
+        System.out.println(Arrays.deepToString(Board.getBoard().tiles));
     }
 }
