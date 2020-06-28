7fbf89e93e21824c62216dc3e73eed9b859a870c
rozenbergidan <idanroze@post.bgu.ac.il>
2020-06-28 11:03:08 +0300

    changed the main function

--- hw3/src/Control/GameController.java
+++ hw3/src/Control/GameController.java
@@ -2,13 +2,20 @@ package Control;
 
 import Board.*;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class GameController {
     public GameController(){}
-    public void Start(String[] levels){
-        Board.getBoard().initBoard(levels);
+    public void Start(String path){
+        Board.getBoard().initBoard(getLevels(path));
         boolean gameOver = false;
         Scanner scan = new Scanner(System.in);
         while (!gameOver){
@@ -17,5 +24,31 @@ public class GameController {
             char playerAct = scan.next().charAt(0);// = ...get the char to the playerd
             gameOver = Board.getBoard().gameTick(playerAct);// send with char
         }
+    }
+    private String[] getLevels(String args){
+        File dir = new File(args);
+        String[] dirFiles = dir.list();
+        assert dirFiles != null;
+        List<String> dirFilesPath = Arrays.stream(dirFiles).map(s -> args + "/" + s).collect(Collectors.toList());
+        List<List<String>> filesContent = new ArrayList<>();
+        for (String filePath : dirFilesPath){
+            try {
+                filesContent.add(Files.readAllLines(Paths.get(filePath)));
+            } catch (IOException e) {
+                e.printStackTrace();
+                System.exit(-1);
+            }
+        }
+        String[] levels=new String[filesContent.size()];
+        int index=0;
+        for (List<String> lst: filesContent) {
+            String str="";
+            for (String r: lst) {
+                str+=r+"\n";
+            }
+            levels[index]=str;
+            index++;
+        }
+        return levels;
     }
 }
--- hw3/src/Control/Main.java
+++ hw3/src/Control/Main.java
@@ -12,29 +12,6 @@ import java.util.stream.Collectors;
 public class Main {
     public static void main(String[]args){
         GameController gc=new GameController();
-        File levelsDir = new File(args[0]);
-        String[] levelsDirFilesNames = levelsDir.list();
-        assert levelsDirFilesNames != null;
-        List<String> inDirFilesPath = Arrays.stream(levelsDirFilesNames).map(s -> args[0] + "/" + s).collect(Collectors.toList());
-        List<List<String>> LevelFilesContent = new ArrayList<>();
-        for (String filePath : inDirFilesPath){
-            try {
-                LevelFilesContent.add(Files.readAllLines(Paths.get(filePath)));
-            } catch (IOException e) {
-                e.printStackTrace();
-                System.exit(-1);
-            }
-        }
-        String[] levels=new String[LevelFilesContent.size()];
-        int index=0;
-        for (List<String> lst: LevelFilesContent) {
-            String str="";
-            for (String r: lst) {
-                str+=r+"\n";
-            }
-            levels[index]=str;
-            index++;
-        }
-        gc.Start(levels);
+        gc.Start(args[0]);
     }
 }
